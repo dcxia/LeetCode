95fede6a47bdb7fc70b42ff740394ef1cfd4219b
Daniel Lu <dcxia.zq@gmail.com>
2019-04-21 17:05:08 -0400

    Daily commit ompleted Symmetric Tree

--- src/EasyProblems/SymmetricTree.java
+++ src/EasyProblems/SymmetricTree.java
@@ -1,22 +1,31 @@
 package EasyProblems;
 import LeetCodeDS.TreeNode;
 
-import java.util.*;
 //Problem 101: Symmetric tree: https://leetcode.com/problems/symmetric-tree/
 public class SymmetricTree {
 
-    public static Stack<Integer> CheckSide(){
+    public static boolean symmetric = true;
 
-        return null;
+    public static boolean checkTrees(TreeNode node1, TreeNode node2){
+        //Checks to see if end nodes are null or not.
+        if (node1 == null && node2 == null){
+            return true;
+        }
+        //checks to see if only one branch side of the tree ends
+        if (node1 == null || node2 == null){
+            return false;
+        }
+        //Checks to see if value are the same, and if values are the same for children of node
+        return ((node1.val == node2.val)&& checkTrees(node1.left,node2.right) && checkTrees(node1.right,node2.left));
     }
 
-    public static void Solution(TreeNode root){
-        if
-
+    public static boolean solution(TreeNode root){
+        if (root == null){
+            return true;
+        }
+        return checkTrees(root.left,root.right);
 
     }
-
-
     public static void main (String[] args){
 
     }
